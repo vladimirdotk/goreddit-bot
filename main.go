09f9b38162b36803e96febc97b43aac2404bf4c6
Vladimir Kivernik <vladimir@kivernik.ru>
2021-08-20 00:25:35 +0300

    Add naive realization of bot

--- main.go
+++ main.go
@@ -4,23 +4,143 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
 	"github.com/vartanbeno/go-reddit/v2/reddit"
 )
 
+const (
+	limit               = 5
+	messageQueueSize    = 100
+	checkRedditDelay    = time.Minute * 1
+	sendToTelegramDelay = time.Second * 10
+
+	redditBaseURL = "https://www.reddit.com/"
+)
+
 func main() {
-	client, err := reddit.NewReadonlyClient()
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		log.Fatal("Error loading .env file")
 	}
-	posts, _, err := client.Subreddit.TopPosts(context.Background(), "golang", &reddit.ListPostOptions{
-		ListOptions: reddit.ListOptions{
-			Limit: 5,
-		},
-		Time: "all",
+
+	ticker := time.NewTicker(checkRedditDelay)
+
+	messageQueue := make(chan reddit.Post, messageQueueSize)
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	go func() {
+		termChan := make(chan os.Signal, 1)
+		signal.Notify(termChan, syscall.SIGTERM, syscall.SIGINT)
+		<-termChan
+		cancelFunc()
+	}()
+
+	redisClient, err := reddit.NewReadonlyClient()
+	if err != nil {
+		log.Fatalf("reddit client error: %v", err)
+	}
+
+	tgbot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
+	if err != nil {
+		log.Fatalf("telegram bot error: %v", err)
+	}
+
+	lastID := ""
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go runTelegram(ctx, &wg, tgbot, messageQueue)
+
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Print("context is done, will shutdown gracefully")
+			break loop
+		default:
+			//
+		}
+
+		select {
+		case <-ticker.C:
+			lastID, err = runReddit(ctx, redisClient, lastID, messageQueue)
+			if err != nil {
+				fmt.Printf("run error: %v", err)
+			}
+		case <-ctx.Done():
+			fmt.Print("context is done, will shutdown gracefully")
+			break loop
+		}
+	}
+
+	wg.Wait()
+}
+
+func runReddit(ctx context.Context, client *reddit.Client, lastID string, messageQueue chan reddit.Post) (string, error) {
+	subreddit := os.Getenv("SUBREDDIT")
+
+	posts, _, err := client.Subreddit.NewPosts(ctx, subreddit, &reddit.ListOptions{
+		Limit: limit,
 	})
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		return "", fmt.Errorf("get new posts subreddit=%s: %v", subreddit, err)
 	}
+
+	if len(posts) == 0 {
+		return lastID, nil
+	}
+
+	for _, post := range posts {
+		if post.FullID == lastID {
+			break
+		}
+		messageQueue <- *post
+	}
+
+	lastPostID := posts[0].FullID
+
 	fmt.Printf("Received %d posts.\n", len(posts))
+	fmt.Printf("LastID: %s\n", lastPostID)
+
+	return lastPostID, nil
+}
+
+func runTelegram(ctx context.Context, wg *sync.WaitGroup, tgbot *tgbotapi.BotAPI, messageQueue chan reddit.Post) {
+	defer wg.Done()
+
+	chatIDRaw := os.Getenv("CHAT_ID")
+	chatID, err := strconv.ParseInt(chatIDRaw, 10, 64)
+	if err != nil {
+		log.Fatalf("convert chat id to int64: %v", err)
+	}
+
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			break loop
+		default:
+			//
+		}
+
+		select {
+		case <-ctx.Done():
+			break loop
+		case post := <-messageQueue:
+			msg := fmt.Sprintf("%s%s", redditBaseURL, post.Permalink)
+			tgMsg := tgbotapi.NewMessage(chatID, msg)
+			if _, err := tgbot.Send(tgMsg); err != nil {
+				fmt.Printf("telegram bot send message: %v\n", err)
+			}
+			time.Sleep(sendToTelegramDelay)
+		}
+	}
 }
